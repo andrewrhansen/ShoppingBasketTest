efa49fe4862c4dcb9556cb77eddea46e905fec41
Andrew Hansen <29931706+andrewrhansen@users.noreply.github.com>
2022-04-08 20:19:12 -0600

    Add project files.

---
+++ DealerOn/Classes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketTest
+{
+    internal class Item
+    {
+        public int Quantity { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public bool IsExempt { get; set; }
+        public bool IsImport { get; set; }
+
+        string[] taxExempt = { "Chocolate bar", "Book", "Packet of headache pills", "Imported box of chocolates" };
+
+        public Item(int quantity, string type, decimal price)
+        {
+            Quantity = quantity;
+            Type = type;
+            Price = price;
+            IsExempt = taxExempt.Contains(type) ? true : false;
+            IsImport = type.Contains("Imported") ? true : false;
+        }
+    }
+
+    internal class Basket
+    {
+        public List<Item> Items { get; set; }
+
+        public Basket()
+        {
+            Items = new List<Item>();
+        }
+    }
+}
---
+++ DealerOn/Store.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingBasketTest
+{
+    class Store
+    {
+        Basket customerOrder = new Basket();
+
+        public void ReadFile()
+        {
+            Console.WriteLine("Enter test #:");
+            string testNumber = Console.ReadLine();
+            string textFile = @$"C:\Users\Username\Desktop\test{testNumber}.txt";
+            using (StreamReader file = new StreamReader(textFile))
+            {
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    string[] orderLine = ln.Split(" at ");
+                    string[] quantityAndType = orderLine[0].Split(new[] { ' ' }, 2);
+                    Item i = new Item(Int32.Parse(quantityAndType[0]), quantityAndType[1], Decimal.Parse(orderLine[1]));
+                    customerOrder.Items.Add(i);
+                }
+                file.Close();
+            }
+        }
+
+        public void CombineItems()
+        {
+            List<int> toDelete = new List<int>();
+            for (int i = 0; i < customerOrder.Items.Count; i++)
+            {
+                if (toDelete.Contains(i))
+                {
+                    continue;
+                }
+                else
+                {
+                    for (int j = i + 1; j < customerOrder.Items.Count; j++)
+                    {
+                        if (customerOrder.Items[i].Type == customerOrder.Items[j].Type)
+                        {
+                            customerOrder.Items[i].Quantity += customerOrder.Items[j].Quantity;
+                            toDelete.Add(j);
+                        }
+                    }
+                }
+                
+            }
+            for (int k = 0; k < toDelete.Count; k++)
+            {
+                customerOrder.Items.RemoveAt(toDelete[k]);
+            }
+        }
+
+        public void TotalOrder()
+        {
+            decimal total = 0;
+            decimal taxes = 0;
+            decimal regTax = (decimal).10;
+            decimal importTax = (decimal).05;
+
+            foreach (Item i in customerOrder.Items)
+            {
+                decimal itemTax = 0;
+                if (i.IsImport)
+                {
+                    itemTax += i.Price * importTax;
+                }
+                itemTax += i.IsExempt ? 0 : i.Price * regTax;
+                itemTax = Math.Ceiling(itemTax * 20) / 20;
+                decimal newPrice = (i.Price + itemTax) * i.Quantity;
+                taxes += itemTax;
+                total += newPrice;
+                if (i.Quantity > 1)
+                {
+                    Console.WriteLine($"{i.Type}: {newPrice} ({i.Quantity} @ {(i.Price+itemTax)})");
+                }
+                else
+                {
+                    Console.WriteLine($"{i.Type}: {newPrice}");
+                }
+            }
+            
+            Console.WriteLine($"Sales Taxes: {String.Format("{0:0.00}", taxes)}");
+            Console.WriteLine($"Total: {total}");
+        }
+
+        static void Main(string[] args)
+        {
+            Store s = new();
+            s.ReadFile();
+            s.CombineItems();
+            s.TotalOrder();  
+        }
+    }
+}
